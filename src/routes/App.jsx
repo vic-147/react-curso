d608362f4a303a4b90786a1f4314ab3617dfb778
vic-147 <dmkorps@gmail.com>
2023-03-09 15:22:40 -0300

    [FIX] qite el . del / en el json

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -23,14 +23,14 @@ const App = () => {
 				<Layout>
 					<Switch>
 						<Route exact path="/" component={Home} />
-						<Route exact path="#/login" component={Login} />
-						<Route exact path="#/password-recovery" component={PasswordRecovery} />
-						<Route exact path="#/send-email" component={SendEmail} />
-						<Route exact path="#/new-password" component={NewPassword} />
-						<Route exact path="#/account" component={MyAccount} />
-						<Route exact path="#/signup" component={CreateAccount} />
-						<Route exact path="#/checkout" component={Checkout} />
-						<Route exact path="#/orders" component={Orders} />
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/password-recovery" component={PasswordRecovery} />
+						<Route exact path="/send-email" component={SendEmail} />
+						<Route exact path="/new-password" component={NewPassword} />
+						<Route exact path="/account" component={MyAccount} />
+						<Route exact path="/signup" component={CreateAccount} />
+						<Route exact path="/checkout" component={Checkout} />
+						<Route exact path="/orders" component={Orders} />
 						<Route path="*" component={NotFound} />
 					</Switch>
 				</Layout>
